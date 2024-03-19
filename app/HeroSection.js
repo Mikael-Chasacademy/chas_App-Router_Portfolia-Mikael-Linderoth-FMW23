46a4497c14c601af823742e1b00e2ba9f5c45602
Mikael-Chasacademy <mikael.linderoth@chasacademy.se>
2024-03-19 14:20:32 +0100

    Update HeroSection.js

--- app/HeroSection.js
+++ app/HeroSection.js
@@ -4,7 +4,7 @@ const HeroSection = () => (
     <div className="container mx-auto px-4 ">
       <h1 className="text-4xl md:text-6xl font-bold mb-4">Mikael Linderoth</h1>
       <p className="text-lg md:text-xl">
-        Am i the frontend developer of your dreams?
+        Am i the frontend developer of your dreams??
       </p>
     </div>
   </section>
